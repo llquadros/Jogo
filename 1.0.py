5d98c8798f1f42a392cb48db3f30e75a616e62e9
eb100 <eb100221@gmail.com>
2020-11-16 16:17:00 -0300

    alteração para teste

--- 1.0.py
+++ 1.0.py
@@ -12,7 +12,7 @@ pygame.display.set_caption('THE ARCHER')
 
 #----------------- Inicia estrutura de dados --------------------#
 game=True
-
+print('oi')
 #----------------- Assets --------------------#
 #imagens
 
